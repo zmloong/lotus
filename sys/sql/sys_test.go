129e95567f1510b13abe27bbcef152664188068a
zmloong <zmloong@126.com>
2024-04-18 00:19:33 +0800

    +

--- sys/sql/sys_test.go
+++ sys/sql/sys_test.go
@@ -9,20 +9,20 @@ import (
 	_ "github.com/denisenkom/go-mssqldb" //sqlservice 驱动
 	_ "github.com/go-sql-driver/mysql"   //mysql 驱动
 	_ "github.com/godror/godror"         //oracle 驱动
-	_ "github.com/lib/pq"  //postgres 驱动
-	lgsql "github.com/zmloong/lotus/sys/sql"
+	_ "github.com/lib/pq"                //postgres 驱动
+	lssql "github.com/zmloong/lotus/sys/sql"
 )
 
 // /测试SqlServer 存储过程
 func Test_SqlServer(t *testing.T) {
-	err := lgsql.OnInit(nil,
-		lgsql.SetSqlType(lgsql.SqlServer),
-		lgsql.SetSqlUrl(fmt.Sprintf("server=%s;port%d;database=%s;user id=%s;password=%s;encrypt=disable", "172.27.100.118", 1433, "test", "sa", "123456")),
+	err := lssql.OnInit(nil,
+		lssql.SetSqlType(lssql.SqlServer),
+		lssql.SetSqlUrl(fmt.Sprintf("server=%s;port%d;database=%s;user id=%s;password=%s;encrypt=disable", "172.27.100.118", 1433, "test", "sa", "123456")),
 	)
 	if err != nil {
 		fmt.Printf("初始化失败=%s", err.Error())
 	} else {
-		row, err := lgsql.QueryContext("Login", sql.Named("@Account", "liwei1dao"), sql.Named("@Password", "li13451234"))
+		row, err := lssql.QueryContext("Login", sql.Named("@Account", "liwei1dao"), sql.Named("@Password", "li13451234"))
 		if err != nil {
 			fmt.Printf("执行存储工程失败:%s", err.Error())
 		} else {
@@ -41,9 +41,9 @@ func Test_SqlServer(t *testing.T) {
 }
 
 func Test_MySql(t *testing.T) {
-	err := lgsql.OnInit(nil,
-		lgsql.SetSqlType(lgsql.MySql),
-		lgsql.SetSqlUrl("root:Idss@sjzt2021@tcp(172.20.27.125:3306)/mysql"),
+	err := lssql.OnInit(nil,
+		lssql.SetSqlType(lssql.MySql),
+		lssql.SetSqlUrl("root:Idss@sjzt2021@tcp(172.20.27.125:3306)/mysql"),
 	)
 	if err != nil {
 		fmt.Printf("初始化失败=%v\n", err)
@@ -51,7 +51,7 @@ func Test_MySql(t *testing.T) {
 		fmt.Printf("初始化成功")
 		// if data, err := Query("select table_name from information_schema.tables where table_schema='mysql' and table_type='base table'"); err == nil {
 		//SELECT * FROM (Select test.*,@rowno:=@rowno+1 as INCREMENTAL From test) AS T WHERE INCREMENTAL >= 0 and INCREMENTAL < 100 order by INCREMENTAL
-		if data, err := lgsql.Query("select test.*,(@rowno:=@rowno+1) as rownum from test, (select @rowno:=0) as init;"); err == nil {
+		if data, err := lssql.Query("select test.*,(@rowno:=@rowno+1) as rownum from test, (select @rowno:=0) as init;"); err == nil {
 			if coluns, err := data.Columns(); err == nil {
 				fmt.Printf("coluns:%v\n", coluns)
 			} else {
@@ -74,9 +74,9 @@ func Test_MySql(t *testing.T) {
 }
 
 func Test_Oracle(t *testing.T) {
-	err := lgsql.OnInit(nil,
-		lgsql.SetSqlType(lgsql.Oracle),
-		lgsql.SetSqlUrl("idss_sjzt/idss1234@172.20.27.125:1521/nek"),
+	err := lssql.OnInit(nil,
+		lssql.SetSqlType(lssql.Oracle),
+		lssql.SetSqlUrl("idss_sjzt/idss1234@172.20.27.125:1521/nek"),
 	)
 	if err != nil {
 		fmt.Printf("初始化失败=%v\n", err)
@@ -86,9 +86,9 @@ func Test_Oracle(t *testing.T) {
 }
 
 func Test_DM(t *testing.T) {
-	err := lgsql.OnInit(nil,
-		lgsql.SetSqlType(lgsql.DM),
-		lgsql.SetSqlUrl("dm://Sysdba:Sysdba@172.20.27.145:5236"),
+	err := lssql.OnInit(nil,
+		lssql.SetSqlType(lssql.DM),
+		lssql.SetSqlUrl("dm://Sysdba:Sysdba@172.20.27.145:5236"),
 	)
 	if err != nil {
 		fmt.Printf("初始化失败=%v\n", err)
